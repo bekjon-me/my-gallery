1f777e56b6c4a800a7a9eccc45adfa59c2a9b2a4
Bekjon Ishpulatov <bekjonishpulatov8@gmail.com>
2025-07-02 18:38:28 +0500

    error management with sentry

--- README.md
+++ README.md
@@ -8,8 +8,8 @@
 - [x] Add authentication (w/ clerk)
 - [x] Add image upload
 - [x] "taint" (server-only)
-- [] Use Next/Image component
-- [] Error management (w/ Sentry)
+- [x] Use Next/Image component
+- [x] Error management (w/ Sentry)
 - [] Routing/image page (parallel route)
 - [] Update upload button to be less cringe
 - [] Analytics (posthog)
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://e7ae104eba4fb22b588e91cf5d91f40f@o4509599167479808.ingest.de.sentry.io/4509599171674192",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
